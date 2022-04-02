e1ef29c38d9b8c2e0f691db8e109b963278a7624
Dillon Hafer <dh@dillonhafer.com>
2022-04-01 22:04:19 -0400

    Add test for name

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,6 +2,10 @@ require "spec_helper"
 
 RSpec.describe Card do
   describe "#new" do
+    let(:card) do
+      described_class.new(card_name: "")
+    end
+
     it "requires a card name" do
       expect {
         described_class.new
@@ -9,11 +13,29 @@ RSpec.describe Card do
     end
 
     it "has a default value of 0" do
-      expect(described_class.new(card_name: "").value).to eq(0)
+      expect(card.value).to eq(0)
     end
 
     it "has a default maki_value of 0" do
-      expect(described_class.new(card_name: "").maki_value).to eq(0)
+      expect(card.maki_value).to eq(0)
+    end
+  end
+
+  describe "#name" do
+    let(:card_name) { "寿司" }
+    let(:card) do
+      described_class.new(card_name:, maki_value: 3)
+    end
+
+    context "when the name includes 'Maki'" do
+      let(:card_name) { "Maki" }
+      it "has a * for each maki value" do
+        expect(card.name).to eq("Maki(***)")
+      end
+    end
+
+    it "returns the card name" do
+      expect(card.name).to eq("寿司")
     end
   end
 end
