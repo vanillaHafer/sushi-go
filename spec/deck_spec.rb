10fbfe0fa4c1fb57cd6d252b98d499a40ecd141d
Dillon Hafer <dh@dillonhafer.com>
2022-04-01 21:27:19 -0400

    Add spec for shuffle

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe Deck do
       deck = described_class.new
       expect(deck.cards.length).to eq(108)
     end
+
+    it "randomizes the cards" do
+      cards_1 = Deck.new.cards.map(&:card_name)
+      cards_2 = Deck.new.cards.map(&:card_name)
+
+      expect(cards_1).not_to eq(cards_2)
+    end
   end
 end
