9f27eb9e6990bb9a53fabd1e9ebcae33f85560d1
Dillon Hafer <dh@dillonhafer.com>
2022-04-01 21:36:41 -0400

    Add spec for deal_cards

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -14,4 +14,27 @@ RSpec.describe Deck do
       expect(cards_1).not_to eq(cards_2)
     end
   end
+
+  describe "#deal_cards" do
+    let(:deck) { described_class.new }
+    let(:players) {
+      [Player.new, Player.new]
+    }
+
+    context "empty cards" do
+      described_class::HAND_SIZE_HASH.each do |k, v|
+        context "when there are #{k} players" do
+          it "reduces the deck by #{k * v}" do
+            expect {
+              deck.deal_cards(k.times.map { Player.new })
+            }.to change {
+              deck.cards.length
+            }.by(
+              -(k * v)
+            )
+          end
+        end
+      end
+    end
+  end
 end
